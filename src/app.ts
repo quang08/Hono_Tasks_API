b56942c0f7ba3704c7bd84d2125ea9c4b4a01090
quang <nguyenthequang080203@gmail.com>
2024-10-20 14:23:49 +0700

    notFounand Error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
 
 const app = new OpenAPIHono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.get('/error', (c) => {
+    c.status(402)
+    throw new Error("Oh No!")
+})
+
+app.notFound(notFound)
+
+app.onError(onError)
 
 export default app;
