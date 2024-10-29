7d50c3ef2861268b53bd604a82128141d0b4805e
quang <nguyenthequang080203@gmail.com>
2024-10-29 20:38:37 +0700

    add log lib and config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { notFound, onError } from "stoker/middlewares";
+import { pinoLogger } from "./middlewares/pino-logger";
 
 const app = new OpenAPIHono();
+app.use(pinoLogger())
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
